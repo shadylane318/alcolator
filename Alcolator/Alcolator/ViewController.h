9c40f418243cdea5a61e1a4286fd4fa7d69a5b94
Daniel Rodas <shadylane318@me.com>
2016-01-11 20:09:26 -0500

    Checkpoint 19 completed

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Daniel Rodas on 1/6/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
