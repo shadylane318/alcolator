830f064e8f90d7aa3e229917836d749ee34051ac
Daniel Rodas <shadylane318@me.com>
2016-03-09 21:23:42 -0500

    Checkpoint-21-completed

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Daniel Rodas on 3/9/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
---
+++ Alcolator/Alcolator/MainMenuViewController.m
@@ -0,0 +1,17 @@
+//
+//  MainMenuViewController.m
+//  Alcolator
+//
+//  Created by Daniel Rodas on 3/9/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+@end
